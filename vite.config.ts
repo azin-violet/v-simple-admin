efec05c3dc66b65b8a79b9a4ba71e467c263094c
Azin <zhuye13974604062@163.com>
2023-01-17 19:34:23 +0800

    add deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // jsx
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import ElementPlus from 'unplugin-element-plus/vite'
 import WindiCss from 'vite-plugin-windicss'
 import Icons from 'unplugin-icons/vite'
-import {resolve} from 'path'
+import { resolve } from 'path'
 
 const root = process.cwd()
 function pathResolve(dir: string) {
@@ -14,23 +14,27 @@ function pathResolve(dir: string) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    ElementPlus(),
-    WindiCss(),
-    Icons({
-      autoInstall: true,
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /\@\//,
-        replacement: `${pathResolve('src')}/`
-      }
-    ]
+export default defineConfig(({ command, mode }) => {
+  const viteEnv = loadEnv(mode, process.cwd())
+  const { VITE_BASE_PATH } = viteEnv
+  return {
+    base: VITE_BASE_PATH,
+    plugins: [
+      vue(),
+      vueJsx(),
+      ElementPlus(),
+      WindiCss(),
+      Icons({
+        autoInstall: true,
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /\@\//,
+          replacement: `${pathResolve('src')}/`,
+        },
+      ],
+    },
   }
 })
-
